f9fc386ad41b2ae0afb482e225bbdd916f454077
Nel-D-Son-Shougaijams <neldson1406122@gmail.com>
2025-01-19 17:25:09 +0530

    2nd commit

--- RestaurantA/settings.py
+++ RestaurantA/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
-import dj_database_url
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +76,16 @@ WSGI_APPLICATION = 'RestaurantA.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD':'cyhsBNgykMFqzAFOfJBaLeYpSQDBZFve',
+        'HOST':'roundhouse.proxy.rlwy.net',
+        'PORT':'12092',
+    }
 }
 
 
