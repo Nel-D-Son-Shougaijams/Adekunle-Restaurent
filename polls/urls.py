799bb72001068e0921f3b29f7e4f4da525f5efed
Nel-D-Son-Shougaijams <neldson1406122@gmail.com>
2025-01-20 10:16:16 +0530

    2nd commit

--- polls/urls.py
+++ polls/urls.py
@@ -35,6 +35,5 @@ urlpatterns = [
     path("confirmed",views.buyssessioncart,name="orde"),
     path("deleteitemsession/",views.deletesessionitem,name="deleteitemsession"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
